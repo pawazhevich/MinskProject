c8a0f17be15e7a3bfbd62b9bba8e6214214822c6
pazhevich <375292703311p@gmail.com>
2018-05-31 16:44:48 +0300

    changes

--- TogetherProject/TogetherProjectApp/src/components/Question.js
+++ TogetherProject/TogetherProjectApp/src/components/Question.js
@@ -1,29 +1,38 @@
 import React,{Component} from 'react'
+import faker from 'faker'
 
 class Question extends Component{
     state={
       firstVar:'',
-      secondVar:''
+      secondVar:'',
+        questiontext:faker.lorem.sentence(),
+        textvar1:faker.lorem.word(),
+        textvar2:faker.lorem.word(),
+        question_num:1,
     }
     componentWillReceiveProps(nextProps){
         this.setState({
             firstVar:'',
-            secondVar:''
+            secondVar:'',
+            questiontext:faker.lorem.sentence(),
+            textvar1:faker.lorem.word(),
+            textvar2:faker.lorem.word(),
+            question_num: this.state.question_num + 1
         })
     }
     render(){
         const {question,nextQuestion,skipFunction}=this.props
 
-        if(question.id===null){
+        if(this.state.question_num===10){
             skipFunction();
             return(<div>.</div>)
         }
         else{
         return(
-            <div>
-                <h4>{question.text}</h4>
-                <input type="radio" name={1.1} key={question.variants[0]} onClick={()=>{this.setState({firstVar:"(selected)",secondVar:null})}} value={true}/>{question.variants[0]} {this.state.firstVar}<br/>
-                <input type="radio" name={1.1} key={question.variants[1]} onClick={()=>{this.setState({secondVar:"(selected)",firstVar:null})}}/>{question.variants[1]} {this.state.secondVar}<br/>
+            <div>{this.state.question_num} / 10
+                <h4>{this.state.questiontext}?</h4>
+                <input type="radio" name={1.1} key={this.state.textvar1} onClick={()=>{this.setState({firstVar:"(selected)",secondVar:null})}} value={true}/>{this.state.textvar1} {this.state.firstVar}<br/>
+                <input type="radio" name={1.1} key={this.state.textvar2} onClick={()=>{this.setState({secondVar:"(selected)",firstVar:null})}}/>{this.state.textvar2} {this.state.secondVar}<br/>
                 <br/><br/>
                 <button onClick={()=>{
                     if(this.state.firstVar||this.state.secondVar)
